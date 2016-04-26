37ce4335e6cd495c4f26fd02726ff351588bf3ea
Moza USANE <mozamimy@quellencode.org>
2016-04-26 22:57:12 +0900

    Implement inventory feature

--- src/inventory_container.jsx
+++ src/inventory_container.jsx
@@ -9,8 +9,10 @@ class InventoryContainer extends Container {
     this._renderImagesArea();
 
     document.addEventListener('keydown', (e) => {
-      if (e.keyCode == InventoryContainer.KEY_CODES["h"]) {
+      if (e.keyCode == Container.KEY_CODES['h']) {
         document.location.href = 'index.html';
+      } else if (e.keyCode == Container.KEY_CODES['d'] && this.currentImageArea != null) {
+        this._removeFromInventory(this.currentImageArea.id);
       }
     });
   }
@@ -22,5 +24,37 @@ class InventoryContainer extends Container {
 
       this._renderSingleImageArea(id, src);
     }
+  }
+
+  _removeFromInventory(giphy_id) {
+    this.currentImageArea.classList.remove('current');
+
+    if (this.currentImageArea.previousSibling) {
+      var afterImageArea = this.currentImageArea.previousSibling;
+    } else if (this.currentImageArea.nextSibling) {
+      var afterImageArea = this.currentImageArea.nextSibling;
+    } else {
+      var afterImageArea = null;
+    }
+
+    this.currentImageArea.parentNode.removeChild(this.currentImageArea);
+    this.currentImageArea = afterImageArea;
+
+    if (this.currentImageArea) {
+      this.currentImageArea.classList.add('current');
+      location.hash = this.currentImageArea.id;
+    }
+
+    localStorage.removeItem(giphy_id);
+
+    document.title = `Removed from the inventory, ${giphy_id}`;
+
+    setTimeout(() => {
+      document.title = this._title();
+    }, 3000);
+  }
+
+  _title() {
+    return 'Selemene; Inventory';
   }
 }
--- src/selemene_container.jsx
+++ src/selemene_container.jsx
@@ -1,57 +1,32 @@
 'use strict';
 
-import * as CopyPaste from 'copy-paste';
-
 class SelemeneContainer extends Container {
   constructor() {
     super();
 
     this.searchTextBox = document.getElementById('search_keyword');
-    this.resultArea = document.getElementById('resut_area');
-    this.currentImageArea = null;
-
     this.searchTextBox.focus();
 
     // TODO: Refactor to stop using raw keycodes
     document.addEventListener('keydown', (e) => {
-      if (e.keyCode == SelemeneContainer.KEY_CODES["Esc"]) {
+      if (e.keyCode == Container.KEY_CODES["Esc"]) {
         this.searchTextBox.blur();
-      } else if (e.keyCode == SelemeneContainer.KEY_CODES["i"]) {
-        if (this._checkActive(this.searchTextBox)) {
-          this.searchTextBox.focus();
-          e.preventDefault();
-        }
-      } else if (e.keyCode == SelemeneContainer.KEY_CODES["j"]) {
-        if (this._checkActive(this.searchTextBox)) {
-          this._moveToNextImage();
-        }
-      } else if (e.keyCode == SelemeneContainer.KEY_CODES["k"]) {
-        if (this._checkActive(this.searchTextBox)) {
-          this._moveToPreviousImage();
-        }
-      } else if (e.keyCode == SelemeneContainer.KEY_CODES["y"]) {
-        if (this.currentImageArea && this._checkActive(this.searchTextBox)) {
-          let textForCopy = '![LGTM](' + this.currentImageArea.firstChild.src + ')';
-          CopyPaste.copy(textForCopy);
-          document.title = 'Copied | ' + textForCopy;
-
-          setTimeout(() => {
-            document.title = 'Selemene; Search';
-          }, 3000);
-        }
-      } else if (e.keyCode == SelemeneContainer.KEY_CODES["l"]) {
-        if (this._checkActive(this.searchTextBox)) {
-          document.location.href = 'inventory.html';
-        }
-      } else if (e.keyCode == SelemeneContainer.KEY_CODES["f"]) {
+      } else if (e.keyCode == Container.KEY_CODES['i'] && this._checkActive(this.searchTextBox)) {
+        this.searchTextBox.focus();
+        e.preventDefault();
+      } else if (e.keyCode == Container.KEY_CODES['f']) {
         if (this._checkActive(this.searchTextBox)) {
           this._addToInventory(this.currentImageArea.id, this.currentImageArea.childNodes[0]);
+        }
+      } else if (e.keyCode == Container.KEY_CODES['l']) {
+        if (this._checkActive(this.searchTextBox)) {
+          document.location.href = 'inventory.html';
         }
       }
     });
 
     this.searchTextBox.addEventListener('keydown', (e) => {
-      if (e.keyCode == SelemeneContainer.KEY_CODES['Enter']) {
+      if (e.keyCode == Container.KEY_CODES['Enter']) {
         this.currentImageArea = null;
 
         let giphy = new Giphy;
@@ -60,38 +35,6 @@ class SelemeneContainer extends Container {
         });
       }
     });
-  }
-
-  _moveToNextImage() {
-    if (this.currentImageArea == null) {
-      this.currentImageArea = this.resultArea.firstChild
-    } else {
-      if (this.currentImageArea.nextSibling) {
-        this.currentImageArea.classList.remove('current');
-        this.currentImageArea = this.currentImageArea.nextSibling;
-      }
-    }
-
-    this.currentImageArea.classList.add('current');
-    location.hash = this.currentImageArea.id;
-  }
-
-  _moveToPreviousImage() {
-    if (this.currentImageArea == null) {
-      this.currentImageArea = this.resultArea.firstChild
-    } else {
-      if (this.currentImageArea.previousSibling) {
-        this.currentImageArea.classList.remove('current');
-        this.currentImageArea = this.currentImageArea.previousSibling;
-      }
-    }
-
-    this.currentImageArea.classList.add('current');
-    location.hash = this.currentImageArea.id;
-  }
-
-  _checkActive(domObject) {
-    return (document.activeElement != domObject);
   }
 
   _renderResult(resources) {
@@ -111,10 +54,14 @@ class SelemeneContainer extends Container {
   _addToInventory(giphy_id, node) {
     localStorage.setItem(giphy_id, node.src);
 
-    document.title = `Added to inventory, ${giphy_id}`;
+    document.title = `Added to the inventory, ${giphy_id}`;
 
     setTimeout(() => {
-      document.title = 'Selemene; Search';
+      document.title = this._title();
     }, 3000);
+  }
+
+  _title() {
+    return 'Selemene; Search';
   }
 }
